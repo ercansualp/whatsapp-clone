0705727431ab9a8fd96a6af51d32a5e5387bc322
Ercan SUALP <ercansualp@outlook.com.tr>
2024-12-13 18:18:12 +0300

    Update url.tsx

--- client/src/url.tsx
+++ client/src/url.tsx
@@ -1,4 +1,4 @@
 export const serverPort = "5000"
-export const serverIP = "13.48.211.224"
+export const serverIP = "217.15.170.41"
 export const userAPI = `http://${serverIP}:${serverPort}/user`
 export const messageAPI = `http://${serverIP}:${serverPort}/message`
